9472f135c293ae24094654dcd65b2ef64bb1dd45
soumavm <soumavm@gmail.com>
2021-12-20 13:09:06 -0700

    Enabled read/write connection with phone

--- ESP_BLE.ino
+++ ESP_BLE.ino
@@ -1,7 +1,7 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
-// used https://www.uuidgenerator.net/
+
 #define SERVICE_UUID "a31e7f39-239b-4ccf-81d3-df44c325716f"
 #define CHARACTERISTIC_UUID "8a8d440c-b1c7-4a97-af26-784211ee50f0"
 
@@ -11,9 +11,13 @@ BLECharacteristic *pOutputChar;
 class InputReceivedCallbacks: 
   public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharWriteState) {
-        uint8_t *inputValues = pCharWriteState->getData();
-        pOutputChar->setValue((uint8_t *)outputData, 1);
-        pOutputChar->notify();
+      
+        std::string response = pCharWriteState->getValue();
+        Serial.print("you entered: ");
+        for(int i=0; i<response.length();i++){
+          Serial.print(response[i]);
+        }
+       
     }
 };
 
@@ -23,18 +27,17 @@ class InputReceivedCallbacks:
 void setup() {
   Serial.begin(115200);
   Serial.println("Establishing server!");
-  BLEDevice::init("ThermostatControl");
+  BLEDevice::init("myESP");
   BLEServer *pServer = BLEDevice::createServer();
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
   BLECharacteristic *pCharacteristic = pService->createCharacteristic(
                                      CHARACTERISTIC_UUID,
                                      BLECharacteristic::PROPERTY_READ |
-                                     BLECharacteristic::PROPERTY_WRITE |
-                                     BLECharacteristic::PROPERTY_WRITE_NR
+                                     BLECharacteristic::PROPERTY_WRITE
                                      );
   pCharacteristic->setCallbacks(new InputReceivedCallbacks());
-//  pCharacteristic->setValue("Switch = off");
+  pService->start();
   
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
   pAdvertising->start();
