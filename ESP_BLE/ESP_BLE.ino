b9dd77dd37d893e263fa3ab7678e26d43fb200f1
soumavm <soumavm@gmail.com>
2021-12-20 18:19:18 -0700

    Added conversion from UTF 8 input to decimal

--- ESP_BLE/ESP_BLE.ino
+++ ESP_BLE/ESP_BLE.ino
@@ -5,19 +5,39 @@
 #define SERVICE_UUID "a31e7f39-239b-4ccf-81d3-df44c325716f"
 #define CHARACTERISTIC_UUID "8a8d440c-b1c7-4a97-af26-784211ee50f0"
 
-static uint8_t outputData[1];
-BLECharacteristic *pOutputChar;
+const byte gpio = 5;
+
+//void types(String a) { Serial.println("it's a String"); }
+//void types(int a) { Serial.println("it's an int"); }
+//void types(char *a) { Serial.println("it's a char*"); }
+//void types(float a) { Serial.println("it's a float"); }
+//void types(bool a) { Serial.println("it's a bool"); }
 
 class InputReceivedCallbacks: 
   public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharWriteState) {
-      
         std::string response = pCharWriteState->getValue();
         Serial.print("you entered: ");
+        int inputTemp=0;
         for(int i=0; i<response.length();i++){
           Serial.print(response[i]);
+          if(response[i]>=48 && response[i] <= 57){
+            if(!inputTemp){
+              inputTemp+= response[i]-48;
+            }
+            else{
+              inputTemp*= 10;
+              inputTemp+= (response[i]-48);
+            }
+          }
         }
-       
+        Serial.print(inputTemp);
+        if(inputTemp){
+          digitalWrite(gpio, HIGH);
+          delay(inputTemp*1000);
+          digitalWrite(gpio, LOW);
+          }
+       Serial.println();
     }
 };
 
@@ -25,6 +45,7 @@ class InputReceivedCallbacks:
 
 
 void setup() {
+  //sets of BLE server
   Serial.begin(115200);
   Serial.println("Establishing server!");
   BLEDevice::init("myESP");
@@ -36,11 +57,14 @@ void setup() {
                                      BLECharacteristic::PROPERTY_READ |
                                      BLECharacteristic::PROPERTY_WRITE
                                      );
+  //sets up Read/Write for server
   pCharacteristic->setCallbacks(new InputReceivedCallbacks());
   pService->start();
-  
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
   pAdvertising->start();
+
+  //sets LED
+  pinMode(gpio, OUTPUT);
 }
 
 void loop() {
